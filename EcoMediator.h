f36dc82994c6e53ed3ddeeb1ca2267cb68584adf
three-stone-chen <15820790320@163.com>
2017-03-14 23:10:26 +0800

    无需注册router

---
+++ EcoMediator.h
@@ -0,0 +1,13 @@
+//
+//  EcoMediator.h
+//  EcoRouterDemo
+//
+//  Created by 陈磊 on 2017/3/14.
+//  Copyright © 2017年 chenlei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EcoMediator : NSObject
+
+@end
---
+++ EcoMediator.m
@@ -0,0 +1,119 @@
+//
+//  EcoMediator.m
+//  EcoRouterDemo
+//
+//  Created by 陈磊 on 2017/3/14.
+//  Copyright © 2017年 chenlei. All rights reserved.
+//
+
+#import "EcoMediator.h"
+
+@interface EcoMediator ()
+
+@property (nonatomic, strong) NSMutableDictionary *cachedTarget;
+
+@end
+
+@implementation EcoMediator
+
+#pragma mark - public methods
++ (instancetype)sharedInstance
+{
+    static EcoMediator *mediator;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        mediator = [[EcoMediator alloc] init];
+    });
+    return mediator;
+}
+
+/*
+ scheme://[target]/[action]?[params]
+ 
+ url sample:
+ aaa://targetA/actionB?id=1234
+ */
+
+- (id)performActionWithUrl:(NSURL *)url completion:(void (^)(NSDictionary *))completion
+{
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    NSString *urlString = [url query];
+    for (NSString *param in [urlString componentsSeparatedByString:@"&"]) {
+        NSArray *elts = [param componentsSeparatedByString:@"="];
+        if([elts count] < 2) continue;
+        [params setObject:[elts lastObject] forKey:[elts firstObject]];
+    }
+    
+    NSString *actionName = [url.path stringByReplacingOccurrencesOfString:@"/" withString:@""];
+    if ([actionName hasPrefix:@"native"]) {
+        return @(NO);
+    }
+    
+    id result = [self performTarget:url.host action:actionName params:params shouldCacheTarget:NO];
+    if (completion) {
+        if (result) {
+            completion(@{@"result":result});
+        } else {
+            completion(nil);
+        }
+    }
+    return result;
+}
+
+- (id)performTarget:(NSString *)targetName action:(NSString *)actionName params:(NSDictionary *)params shouldCacheTarget:(BOOL)shouldCacheTarget
+{
+    
+    NSString *targetClassString = [NSString stringWithFormat:@"Target_%@", targetName];
+    NSString *actionString = [NSString stringWithFormat:@"Action_%@:", actionName];
+    
+    id target = self.cachedTarget[targetClassString];
+    if (target == nil) {
+        Class targetClass = NSClassFromString(targetClassString);
+        target = [[targetClass alloc] init];
+    }
+    
+    SEL action = NSSelectorFromString(actionString);
+    
+    if (target == nil) {
+                return nil;
+    }
+    
+    if (shouldCacheTarget) {
+        self.cachedTarget[targetClassString] = target;
+    }
+    
+    if ([target respondsToSelector:action]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        return [target performSelector:action withObject:params];
+#pragma clang diagnostic pop
+    } else {
+        SEL action = NSSelectorFromString(@"notFound:");
+        if ([target respondsToSelector:action]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            return [target performSelector:action withObject:params];
+#pragma clang diagnostic pop
+        } else {
+            [self.cachedTarget removeObjectForKey:targetClassString];
+            return nil;
+        }
+    }
+}
+
+- (void)releaseCachedTargetWithTargetName:(NSString *)targetName
+{
+    NSString *targetClassString = [NSString stringWithFormat:@"Target_%@", targetName];
+    [self.cachedTarget removeObjectForKey:targetClassString];
+}
+
+#pragma mark - getters and setters
+- (NSMutableDictionary *)cachedTarget
+{
+    if (_cachedTarget == nil) {
+        _cachedTarget = [[NSMutableDictionary alloc] init];
+    }
+    return _cachedTarget;
+}
+
+@end
